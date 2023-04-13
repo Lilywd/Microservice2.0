9a6b00665b0381d54a16825338dba3167754e23e
Popsicool <akinolasamson1234@gmail.com>
2023-04-13 20:39:02 +0100

    added url test case

--- Backend/User/app/urls.py
+++ Backend/User/app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.ping, name="ping"),
     path("isAdmin", views.IsAdmin, name="is-admin"),
     path("isStaff", views.IsStaff, name="is-staff"),
+    path("deleteUser", views.deleteUser, name="delete-user"),
     path("addAdmin", views.addAdmin, name="add-admin"),
     path("removeAdmin", views.removeAdmin, name="remove-admin"),
     path("addStaff", views.addStaff, name="add-staff"),
