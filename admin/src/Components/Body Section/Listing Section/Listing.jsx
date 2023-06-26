c567f84a12f2624e994c7fde288bc649f2f212cc
SamuelNgundi <samuelwngundi02@gmail.com>
2023-06-26 22:16:36 +0300

    Listings page fix 8

--- admin/src/Components/Body Section/Listing Section/Listing.jsx
+++ admin/src/Components/Body Section/Listing Section/Listing.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './listing.css'
+import {BsArrowRightShort} from 'react-icons/bs'
+import { AiFillHeart } from 'react-icons/ai'
+
+import img from '../../../Assets/candle1.jpg'
 
 const Listing = () => {
   return (
-    <div>
-      Listing
+    <div className='listingSection'>
+      <div className="heading flex">
+        <h1>My Listings</h1>
+        <button className='btn flex'>
+          See All <BsArrowRightShort className='icon'/>
+        </button>
+      </div>
+
+      <div className="secContainer flex">
+        <div className="singleItem">
+          <AiFillHeart className='icon'/>
+          <img src={img} alt="Image here" />
+          <h3>Annual LC</h3>
+        </div>
+      </div>
     </div>
   )
 }
